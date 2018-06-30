f44d7dba2f42ddcf8d3d7b351347d713135a5ee2
Steven Reitsma <steven@properchaos.nl>
2018-06-30 17:42:06 +0200

    Ingress

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,1 +1,16 @@
-print("Hello world")
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Match(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+api.add_resource(Match, '/kickerscore/api/v1/match')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
