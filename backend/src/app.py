b957c3ed292a914a12f4008a223ab96350871725
DJLemkes <diederik.jan.lemkes@bigdatarepublic.nl>
2018-10-25 21:55:01 +0200

    Add Slack based players setup

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,22 +1,34 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from match import MatchResource, MatchesResource, AnalyzePlayers, AnalyzeTeams
 from player import PlayerResource, PlayersResource
 from db import *
+from slack_sync import sync_new_left_channel_members, sync_existing_members_info
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
+app.config['SQLALCHEMY_TRAdCK_MODIFICATIONS'] = False
 api = Api(app)
 cors = CORS(app)
 db.init_app(app)
+db.app = app
 
 migrate = Migrate(app, db)
+
+# Watch it: this stuff will get out of control if you run multiple
+# instances of this app. Need to ensure there's just one scheduler!
+scheduler = BackgroundScheduler()
+# Check channel if there are new players
+new_player_sync = scheduler.add_job(sync_new_left_channel_members, "interval", seconds=30)
+# Revisist existing players and update info if required
+existing_player_sync = scheduler.add_job(sync_existing_members_info, "interval", minutes=1)
+scheduler.start()
 
 
 class Healthz(Resource):
--- backend/src/models.py
+++ backend/src/models.py
@@ -6,11 +6,11 @@ from sqlalchemy import func, desc
 class Match(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    blue_offense_player = db.Column(db.String(32), db.ForeignKey('player.username'))
-    blue_defense_player = db.Column(db.String(32), db.ForeignKey('player.username'))
+    blue_offense_player = db.Column(db.String(32), db.ForeignKey('player.slack_id'))
+    blue_defense_player = db.Column(db.String(32), db.ForeignKey('player.slack_id'))
 
-    red_offense_player = db.Column(db.String(32), db.ForeignKey('player.username'))
-    red_defense_player = db.Column(db.String(32), db.ForeignKey('player.username'))
+    red_offense_player = db.Column(db.String(32), db.ForeignKey('player.slack_id'))
+    red_defense_player = db.Column(db.String(32), db.ForeignKey('player.slack_id'))
 
     blue_offense_player_skill_gain_overall = db.Column(db.Float)
     blue_defense_player_skill_gain_overall = db.Column(db.Float)
@@ -18,7 +18,7 @@ class Match(db.Model):
     blue_offense_player_skill_gain_offense = db.Column(db.Float)
     blue_defense_player_skill_gain_defense = db.Column(db.Float)
 
-    red_offense_player_skill_gain_overall = db.Column(db.Float) 
+    red_offense_player_skill_gain_overall = db.Column(db.Float)
     red_defense_player_skill_gain_overall = db.Column(db.Float)
 
     red_offense_player_skill_gain_offense = db.Column(db.Float)
@@ -73,8 +73,11 @@ class Match(db.Model):
 
 
 class Player(db.Model):
-    username = db.Column(db.String(32), primary_key=True)
+    slack_id = db.Column(db.String(16), primary_key=True)
+    slack_username = db.Column(db.String(64))
+    slack_avatar = db.Column(db.Text)
     registration_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    active = db.Column(db.Boolean, nullable=False, default=True)
 
     rating_mu = db.Column(db.Float, nullable=False)
     rating_sigma = db.Column(db.Float, nullable=False)
@@ -85,18 +88,39 @@ class Player(db.Model):
     rating_mu_defense = db.Column(db.Float, nullable=False)
     rating_sigma_defense = db.Column(db.Float, nullable=False)
 
+    def with_updated_slack_info(self, username: str, avatar: str):
+        self.slack_avatar = avatar
+        self.slack_username = username
+        return self
+
     def serialize(self):
-        sub = db.session.query(Player.username, func.row_number().over(order_by=desc(Player.rating_mu)).label('pos')).subquery()
-        pos = db.session.query(sub.c.pos).filter(sub.c.username == self.username).scalar() - 1
+        sub = db.session.query(
+            Player.slack_username,
+            func.row_number().over(order_by=desc(Player.rating_mu)).label('pos')
+        ).subquery()
+        pos = db.session.query(sub.c.pos) \
+            .filter(sub.c.slack_username == self.slack_username) \
+            .scalar() - 1
 
-        sub = db.session.query(Player.username, func.row_number().over(order_by=desc(Player.rating_mu_offense)).label('pos')).subquery()
-        pos_offense = db.session.query(sub.c.pos).filter(sub.c.username == self.username).scalar() - 1
+        sub = db.session.query(
+            Player.slack_username, func.row_number()
+            .over(order_by=desc(Player.rating_mu_offense)).label('pos')
+        ).subquery()
+        pos_offense = db.session.query(sub.c.pos) \
+            .filter(sub.c.slack_username == self.slack_username) \
+            .scalar() - 1
 
-        sub = db.session.query(Player.username, func.row_number().over(order_by=desc(Player.rating_mu_defense)).label('pos')).subquery()
-        pos_defense = db.session.query(sub.c.pos).filter(sub.c.username == self.username).scalar() - 1
+        sub = db.session.query(
+            Player.slack_username, func.row_number()
+            .over(order_by=desc(Player.rating_mu_defense)).label('pos')
+        ).subquery()
+        pos_defense = db.session.query(sub.c.pos) \
+            .filter(sub.c.slack_username == self.slack_username) \
+            .scalar() - 1
 
         return {
-            "username": self.username,
+            "username": self.slack_username,
+            "avatar": self.slack_avatar,
             "registration_date": str(self.registration_date),
             "current_trueskill": {
                 "overall": self.rating_mu,
